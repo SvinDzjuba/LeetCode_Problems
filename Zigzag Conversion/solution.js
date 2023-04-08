d4aa53836a00d16d30fcc90d1203ea9f28877935
Maksim Dzjubenko <90320393+SvinDzjuba@users.noreply.github.com>
2023-04-08 15:09:46 +0300

    GET OVER HERE! Zigzag conversion is done.

--- Zigzag Conversion/solution.js
+++ Zigzag Conversion/solution.js
@@ -3,15 +3,33 @@
  * @param {number} numRows
  * @return {string}
  */
-var convert = function(s, numRows) {
+var convert = function (s, numRows) {
     let zigZagString = '';
-    let generalArray = [];
-    let columnNumber = 1;
-    
+    let charsArr = [];
+    let charPosHandler = 0;
+    let orientation = 1;
+
     for (let i = 0; i < s.length; i++) {
-        if(i + numRows)
-        generalArray.push(s[]);
+        if (numRows > 1) {
+            orientation % 2 === 0 ? charPosHandler-- : charPosHandler++;
+            if (i == ((numRows - 1) * orientation)) {
+                orientation++;
+            }
+        }
+        charsArr.push(
+            {
+                line: charPosHandler,
+                char: s[i]
+            }
+        );
+    }
+    for (let j = 0; j <= numRows; j++) {
+        for (let i = 0; i < charsArr.length; i++) {
+            if (charsArr[i].line == j) {
+                zigZagString += charsArr[i].char;
+            }
+        }
     }
     return zigZagString;
 };
-console.log(convert("PAYPALISHIRING", 4));
+console.log(convert("AB", 1));
